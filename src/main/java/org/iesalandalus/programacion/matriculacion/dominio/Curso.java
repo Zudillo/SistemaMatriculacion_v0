8168d856e5e6508dc70ef495dd7c03012fe87eb4
Zudillo <fj.glezvillaverde@outlook.com>
2024-12-15 16:45:16 +0100

    Clase alumno

---
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Curso.java
@@ -0,0 +1,22 @@
+package org.iesalandalus.programacion.matriculacion.dominio;
+
+public enum Curso {
+
+    PRIMERO("Primero"),
+    SEGUNDO("Segundo");
+
+    private final String cadenaAMostrar;
+
+    Curso(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    public String imprimir() {
+        return this.ordinal() + ".-" + this.cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return cadenaAMostrar;
+    }
+}
